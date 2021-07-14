a763255ce515586fb4f7b5421952f11316088528
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2021-07-14 21:49:42 +0200

    :memo: enable cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import productRouter from "./products/index.js";
 import { errorMiddleWare } from "./middlewares/index.js";
+import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/api/products", productRouter);
 
