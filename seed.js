ef36743bb4528999e1ecffeda04c308522b87cac
AbdelRahman Hussien Mahmoud <abdelrahman.h.mahmoued@gmail.com>
2021-07-15 09:49:29 +0200

    :bug: fix data seeding

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 import productModel from "./src/products/productModel.js";
 import db from "./data/db.json";
 export default async function seed() {
-  const size = productModel.estimatedDocumentCount();
+  const size = await productModel.estimatedDocumentCount();
   if(size === 0){
     await productModel.insertMany(db.products);
     console.log("Data Inserted");
